fcff45ebfcaab2f145439eb957ebe3953c625c7f
Zhenji Yang <abcdnned@126.com>
2025-07-15 23:54:59 +0800

    add damage logic

--- src/collisions.rs
+++ src/collisions.rs
@@ -1,13 +1,48 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
+use crate::damage::Damage;
+use crate::unit::Hp;
+use crate::enemy::Enemy;
 
 pub fn handle_collisions(
     mut collision_events: EventReader<CollisionEvent>,
+    damage_query: Query<&Damage>,
+    mut hp_query: Query<&mut Hp>,
+    enemy_query: Query<&Enemy>,
 ) {
     for collision_event in collision_events.read() {
         match collision_event {
             CollisionEvent::Started(entity1, entity2, _) => {
-                println!("Collision started between {:?} and {:?}", entity1, entity2);
+                // Check if entity1 has damage and entity2 is an enemy with HP
+                if let (Ok(damage), Ok(mut hp)) = (
+                    damage_query.get(*entity1),
+                    hp_query.get_mut(*entity2)
+                ) {
+                    if enemy_query.get(*entity2).is_ok() {
+                        let damage_amount = damage.get_amount();
+                        let old_hp = hp.hp;
+                        
+                        hp.hp = (hp.hp - damage_amount).max(0.0);
+                        
+                        println!("Sword hit! Damage: {:.1} | HP: {:.1} -> {:.1}", 
+                            damage_amount, old_hp, hp.hp);
+                    }
+                }
+                // Check the reverse case (entity2 has damage, entity1 is enemy)
+                else if let (Ok(damage), Ok(mut hp)) = (
+                    damage_query.get(*entity2),
+                    hp_query.get_mut(*entity1)
+                ) {
+                    if enemy_query.get(*entity1).is_ok() {
+                        let damage_amount = damage.get_amount();
+                        let old_hp = hp.hp;
+                        
+                        hp.hp = (hp.hp - damage_amount).max(0.0);
+                        
+                        println!("Sword hit! Damage: {:.1} | HP: {:.1} -> {:.1}", 
+                            damage_amount, old_hp, hp.hp);
+                    }
+                }
             }
             CollisionEvent::Stopped(entity1, entity2, _) => {}
         }
