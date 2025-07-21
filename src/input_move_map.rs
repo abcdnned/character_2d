47b2005752b0cc80ba0173b78ba03eeab4bb2e86
Tom Yang <tom.yang01@sap.com>
2025-07-21 17:13:25 +0800

    rename

--- src/input_move_map.rs
+++ src/input_move_map.rs
@@ -1,13 +1,13 @@
 use bevy::prelude::*;
 
 pub fn input_map_to_move(
-    player: Single<(Entity, &mut Transform), With<crate::sword::Sword>>,
+    sword: Single<(Entity, &mut Transform), With<crate::sword::Sword>>,
     mut action_events: EventReader<crate::input::ActionEvent>,
     mut move_events: EventWriter<crate::r#move::ExecuteMoveEvent>,
 ) {
     for _action_event in action_events.read() {
         move_events.write(crate::r#move::ExecuteMoveEvent {
-            entity: player.0,
+            entity: sword.0,
             move_name: "SwingLeft".to_string(),
         });
     }
