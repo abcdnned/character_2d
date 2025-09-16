57a3e0649c5340f288786900390519e8b7cfd813
Zhenji Yang <abcdnned@126.com>
2025-09-16 16:19:50 +0800

    add input map for tunado move

--- src/input_move_map.rs
+++ src/input_move_map.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
 
-use crate::{constants::*, global_entity_map::GlobalEntityMap, weapon::Weapon};
+use crate::{berserker::Berserker, constants::*, global_entity_map::GlobalEntityMap, weapon::Weapon};
 
 pub fn input_map_to_move(
     mut action_events: EventReader<crate::input::ActionEvent>,
     mut move_events: EventWriter<crate::custom_move::ExecuteMoveEvent>,
     global_entities: ResMut<GlobalEntityMap>,
+    berserker_query: Query<&Berserker>,
 ) {
     for action_event in action_events.read() {
         if let Some(weapon) = global_entities.player_weapon.get(&action_event.entity) {
@@ -18,7 +19,18 @@ pub fn input_map_to_move(
                     SWORD_STUB.to_string(),
                     crate::custom_move::MoveInput::Attack,
                 ),
-                ACTION_SPECIAL => (SPIN_LEFT.to_string(), crate::custom_move::MoveInput::Attack),
+                ACTION_SPECIAL => {
+                    // Check if the entity has Berserker component and is level 1
+                    if let Ok(berserker) = berserker_query.get(action_event.entity) {
+                        if berserker.level == 1 {
+                            (TUNADO.to_string(), crate::custom_move::MoveInput::Attack)
+                        } else {
+                            (SPIN_LEFT.to_string(), crate::custom_move::MoveInput::Attack)
+                        }
+                    } else {
+                        (SPIN_LEFT.to_string(), crate::custom_move::MoveInput::Attack)
+                    }
+                },
                 _ => continue, // Skip unknown action types
             };
 
